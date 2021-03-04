3873ef653d018911219bd66e7767c50e1bed6691
Jesús Díaz Artiaga <jesus.diaz@howler.events>
2021-03-04 11:36:29 +0100

    chore: add validations to sightings

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,3 +1,8 @@
 class Sighting < ApplicationRecord
   belongs_to :animal
+
+  validates :spotter, presence: true
+  validates :date, presence: true
+  validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90}
+  validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180}
 end
